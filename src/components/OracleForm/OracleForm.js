00522b99c8df6dca9f20af0c021219db19887ede
manojbabugcm <manojbabugcm@gmail.com>
2022-01-09 22:40:46 +0530

    feat: Update

--- src/components/OracleForm/OracleForm.js
+++ src/components/OracleForm/OracleForm.js
@@ -4,6 +4,8 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './OracleForm.css'
+import { TextField, Grid, Autocomplete, RadioGroup, FormLabel, FormControlLabel, Radio, FormHelperText } from '@mui/material';
 
 export class OracleForm extends Component {
 
@@ -15,21 +17,25 @@ export class OracleForm extends Component {
                 email: '',
                 password: '',
                 rePassword: '',
-                country: 'india',
+                country: '',
                 firstName: '',
                 lastName: '',
                 gender: '',
                 jobTitle: '',
                 workPhone: '',
                 companyName: '',
-                address: '',
+                doorNo: '',
+                street: '',
                 city: '',
-                state: 'tamilnadu',
-                zipcode: '',
-            }
+                state: '',
+                pincode: '',
+            },
+            countries: ['India', 'UK', 'US'],
+            states: ['Tamilnadu', 'Kerala', 'Andra'],
+            // formIsValid: true,
+            errors: {},
         }
     }
-
     handleChange = (e) => {
         const tempObj = this.state.formObj;
         tempObj[e.target.name] = e.target.value;
@@ -39,94 +45,165 @@ export class OracleForm extends Component {
     }
 
     handleSubmit = (e) => {
-        this.setState({ showResult: true });
         e.preventDefault();
+        if (this.handleFormValidation()) {
+            this.setState({ showResult: true });
+        }
+    }
+
+    handleBack = () => {
+        this.setState({ showResult: false });
+    }
+
+    handleFormValidation = () => {
+        let { formObj } = this.state;
+        let formIsValid = true;
+        let errors = {};
+
+        if (!formObj["email"] || typeof formObj["email"] === "undefined" || !formObj["email"].match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+            formIsValid = false;
+            errors["email"] = "Please enter valid email";
+        }
+        if (!formObj["firstName"] || typeof formObj["firstName"] === "undefined" || !formObj["firstName"].match(/^[a-zA-Z ]+$/)) {
+            formIsValid = false;
+            errors["firstName"] = "Please enter valid name";
+        }
+        if (!formObj["lastName"] || typeof formObj["lastName"] === "undefined" || !formObj["lastName"].match(/^[a-zA-Z ]+$/)) {
+            formIsValid = false;
+            errors["lastName"] = "Please enter valid name";
+        }
+        if (!formObj["gender"] || typeof formObj["gender"] === "undefined") {
+            formIsValid = false;
+            errors["gender"] = "Please select valid option";
+        }
+        if (!formObj["pincode"] || typeof formObj["pincode"] === "undefined" || !formObj["pincode"].match(/^^[1-9][0-9]{5}$/)) {
+            formIsValid = false;
+            errors["pincode"] = "Please enter valid pincode";
+        }
+
+        console.log(errors);
+        this.setState({ errors: errors });
+        return formIsValid;
     }
 
     render() {
         const { formObj } = this.state;
+        const { errors } = this.state;
         return <div>
-            <Container>
-                <Row>
-                    <Col>
-                        <Form onSubmit={this.handleSubmit} sm="2" className="justify-content-md-center">
-                            <Form.Group className="mb-3">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control name="email" type="email" value={formObj.email} placeholder="Enter email" onChange={this.handleChange} />
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control name="password" type="Password" value={formObj.password} placeholder="Password" onChange={this.handleChange} />
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Retype password:</Form.Label>
-                                <Form.Control name="rePassword" type="Password" value={formObj.rePassword} placeholder="Password" onChange={this.handleChange} />
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Select aria-label="Default select example" name="country" value={formObj.country} onChange={this.handleChange}>
-                                    <option value="india">India</option>
-                                    <option value="UK">UK</option>
-                                    <option value="US">US</option>
-                                    <option value="france">France</option>
-                                </Form.Select>
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Name</Form.Label>
-                                <Form.Control name="firstName" type="text" value={formObj.firstName} placeholder="First Name" onChange={this.handleChange} />
-                                <Form.Control name="lastName" type="text" value={formObj.lastName} placeholder="Last Name" onChange={this.handleChange} />
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Gender</Form.Label>
-                                <Form.Check name="gender" type="radio" value='male' label="male" checked={this.state.formObj['gender'] === 'male'} onChange={this.handleChange} />
-                                <Form.Check name="gender" type="radio" label="female" value='female' checked={this.state.formObj['gender'] === 'female'} onChange={this.handleChange} />
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Job Title</Form.Label>
-                                <Form.Control name="jobTitle" type="text" value={formObj.jobTitle} placeholder="Job Title" onChange={this.handleChange} />
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Work Phone</Form.Label>
-                                <Form.Control name="workPhone" type="text" value={formObj.workPhone} placeholder="Work Phone" onChange={this.handleChange} />
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Address</Form.Label>
-                                <Form.Control as="textarea" rows="2" name="address" type="text" value={formObj.address} placeholder="Address" onChange={this.handleChange} />
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>City</Form.Label>
-                                <Form.Control name="city" type="text" value={formObj.city} placeholder="City" onChange={this.handleChange} />
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Select aria-label="Default select example" name="state" value={formObj.state} onChange={this.handleChange}>
-                                    <option value="kerala">Kerala</option>
-                                    <option value="tamilnadu">Tamil nadu</option>
-                                    <option value="kernadaga">Kernadaga</option>
-                                    <option value="andhra">Andhra</option>
-                                </Form.Select>
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>ZIP/Postal Code</Form.Label>
-                                <Form.Control name="zipcode" type="text" value={formObj.zipcode} placeholder="Zipcode" onChange={this.handleChange} />
-                            </Form.Group>
-
-                            <Button variant="primary" type="submit">
-                                Creat Account
-                            </Button>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
-
+            {!this.state.showResult && <div>
+                <div className='formHeading d-flex justify-content-center'>
+                    <p>
+                        Create Your Oracle Account
+                    </p>
+                </div>
+                <div className='form d-flex justify-content-center'>
+                    <Container fluid>
+                        <Row>
+                            <Col md lg sm xs={8} >
+                                <Form onSubmit={this.handleSubmit} sm="2" className="justify-content-md-center">
+                                    <Form.Group className="mb-3">
+                                        <Grid container spacing={3}>
+                                            <Grid item xs={12}>
+                                                <TextField fullWidth id="standard-basic email" label="Enter email" variant="standard" name="email" type="text" value={formObj.email} onChange={this.handleChange} helperText={errors.email} />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6}>
+                                                <TextField fullWidth id="standard-basic password" label="Password" variant="standard" type="password" name="password" type="Password" value={formObj.password} onChange={this.handleChange} helperText={errors.password} />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6}>
+                                                <TextField fullWidth id="standard-basic rePassword" label="Retype password" variant="standard" type="password" name="rePassword" type="Password" value={formObj.rePassword} onChange={this.handleChange} helperText={errors.rePassword} />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6}>
+                                                <TextField fullWidth id="standard-basic firstName" label="First Name" variant="standard" name="firstName" type="text" value={formObj.firstName} onChange={this.handleChange} helperText={errors.firstName} />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6}>
+                                                <TextField fullWidth id="standard-basic lastName" label="Last Name" variant="standard" name="lastName" type="text" value={formObj.lastName} onChange={this.handleChange} helperText={errors.lastName} />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6}>
+                                                <FormLabel component="legend">Gender</FormLabel>
+                                                <RadioGroup row aria-label="gender" name="row-radio-buttons-group">
+                                                    <FormControlLabel value="male" control={<Radio />} label="Male" name="gender" checked={this.state.formObj['gender'] === 'male'} onChange={this.handleChange} />
+                                                    <FormControlLabel value="female" control={<Radio />} label="Female" name="gender" checked={this.state.formObj['gender'] === 'female'} onChange={this.handleChange} />
+                                                    <FormHelperText>{errors.gender}</FormHelperText>
+                                                </RadioGroup>
+                                            </Grid>
+                                            <Grid item xs={12} sm={6}>
+                                                <TextField fullWidth id="standard-basic workPhone" label="Work Phone" variant="standard" name="workPhone" type="number" value={formObj.workPhone} onChange={this.handleChange} />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6}>
+                                                <TextField fullWidth id="standard-basic companyName" label="Company name" variant="standard" name="companyName" type="text" value={formObj.companyName} onChange={this.handleChange} />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6}>
+                                                <TextField fullWidth id="standard-basic jobTitle" label="Job Title" variant="standard" name="jobTitle" type="text" value={formObj.jobTitle} onChange={this.handleChange} />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6}>
+                                                <TextField fullWidth id="standard-basic doorNo" label="Door No." variant="standard" name="doorNo" type="text" value={formObj.doorNo} onChange={this.handleChange} />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6}>
+                                                <TextField fullWidth id="standard-basic street" label="Street Name" variant="standard" name="street" type="text" value={formObj.street} onChange={this.handleChange} />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6}>
+                                                <Autocomplete
+                                                    id="standard-basic country"
+                                                    options={this.state.countries}
+                                                    renderInput={(params) => <TextField {...params} label="Country" fullWidth id="standard-basic" name="country" value={params} onChange={this.handleChange} />} />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6}>
+                                                <Autocomplete
+                                                    id="standard-basic state"
+                                                    options={this.state.states}
+                                                    renderInput={(params) => <TextField {...params} label="State" fullWidth id="standard-basic" name="state" value={params} onChange={this.handleChange} />} />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6}>
+                                                <TextField fullWidth id="standard-basic city" label="City" variant="standard" name="city" type="text" value={formObj.city} onChange={this.handleChange} />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6}>
+                                                <TextField fullWidth id="standard-basic pincode" label="Pincode" variant="standard" name="pincode" type="text" value={formObj.pincode} onChange={this.handleChange} helperText={errors.pincode} />
+                                            </Grid>
+                                        </Grid>
+                                    </Form.Group>
+                                    <div className='d-flex justify-content-end'>
+                                        <Button type="submit" className='submitButton' >Creat Account</Button>
+                                    </div>
+                                </Form>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </div>}
 
             {this.state.showResult &&
                 <div >
-                    {Object.keys(this.state.formObj).map((i, index) => {
-                        return <div key={index}>{i}:{this.state.formObj[i]}</div>
-                    })}
+                    <div className='formHeading d-flex justify-content-center'>
+                        <p>
+                            Account Details
+                        </p>
+                    </div>
+                    <div className='d-flex justify-content-start'>
+                        <Button color="secondary" className='backButton' onClick={this.handleBack} >Back</Button>
+                    </div>
+                    <div className='infoTable'>
+                        <Table striped hover>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Key</th>
+                                    <th>Value</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {Object.keys(this.state.formObj).map((i, index) => {
+                                    return <tr>
+                                        <td>{index + 1}</td>
+                                        <td>{i}</td>
+                                        <td>{this.state.formObj[i]}</td>
+                                    </tr>
+                                })}
+                            </tbody>
+                        </Table>
+                    </div>
                 </div>}
         </div >
     }
-
 }
 
 export default OracleForm
